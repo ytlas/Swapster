90019c2655076850e6ba681fbd022bac42794f71
Artemio Pánuco <61919272+whoisarte@users.noreply.github.com>
2023-08-28 23:10:56 -0600

    UPDATE: NavigationBarButtons Change image icon in ViewController

---
+++ Swapster/Swapster/Cells/FriendNotificationCell.swift
@@ -0,0 +1,21 @@
+//
+//  FriendNotificationCell.swift
+//  Swapster
+//
+//  Created by Artemio Pánuco on 28/08/23.
+//
+
+import UIKit
+
+class FriendNotificationCell: UITableViewCell {
+    static let identifier: String = "FriendNotificationCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
--- Swapster/Swapster/ViewController.swift
+++ Swapster/Swapster/ViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableViewFriends: UITableView!
+    //pawprint
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureTableView()
+        configureBarButtons()
+    }
+    
+    func configureBarButtons() {
+        //TODO: Change titles, images, icons, titleColors in this method
+        let newMessageImage = UIImage(systemName: "pawprint")
+        newMessageImage?.withTintColor(.white)
+        let newMessageButton = UIBarButtonItem(title: "", image: newMessageImage, target: self, action: #selector(newMessageTapped))
+        newMessageButton.tintColor = .white
+        
+        let addFriendImage = UIImage(systemName: "arrow.down.message.fill")
+        addFriendImage?.withTintColor(.white)
+        let addFriendButton = UIBarButtonItem(title: "", image: addFriendImage, target: self, action: #selector(newMessageTapped))
+        newMessageButton.tintColor = .white
+        
+        
+        navigationItem.rightBarButtonItems = [addFriendButton, newMessageButton]
+        
+    }
+    
+    @objc func newMessageTapped() {
+        print("Tap on new message...")
+    }
+    
+    func configureTableView() {
+        //tableViewFriends.dataSource = self
+        //tableViewFriends.delegate = self
+        tableViewFriends.register(UINib(nibName: FriendNotificationCell.identifier, bundle: nil), forCellReuseIdentifier: FriendNotificationCell.identifier)
     }
 
 
